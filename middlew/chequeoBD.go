45fe602c3a669a9f451f1cd6ada19ee55ac5f58e
Byron Estrada <bestrada1981@gmail.com>
2021-09-26 06:58:09 -0600

    ver usuario perfil

--- middlew/chequeoBD.go
+++ middlew/chequeoBD.go
@@ -6,11 +6,11 @@ import (
 	"github.com/baemestrada-source/golang-redsocial/bd"
 )
 
-/*ChequeoBD es el midlware que permite conocer el estado de la bd*/
+/*ChequeoBD es el middlew que me permite conocer el estado de la BD */
 func ChequeoBD(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if bd.ChequeoConnection() == 0 {
-			http.Error(w, "Conexion perdida con la Base de datos", 500)
+			http.Error(w, "Conexión perdida con la Base de Datos", 500)
 			return
 		}
 		next.ServeHTTP(w, r)
